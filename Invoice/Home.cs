d540494a055bc13c461fab6aab3d638214776227
Data-Men <nee271raj@protonmail.com>
2023-07-26 20:53:51 +0530

    Export Half

--- Invoice/Home.cs
+++ Invoice/Home.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic;
 using System.Reflection;
 using Excel = Microsoft.Office.Interop.Excel;
 namespace Invoice
@@ -13,7 +14,7 @@ namespace Invoice
         //Class Variables
         Dictionary<string, string> states = new Dictionary<string, string>();
 
-
+        
         private void Home_Load(object sender, EventArgs e)
         {
             //Adding Value in Dictionary
@@ -61,9 +62,11 @@ namespace Invoice
             Excel._Workbook oWB;
             Excel._Worksheet oSheet;
             Excel.Range oRng;
+            try{
+                int xRow = 1;
+                int xCol = 1;
 
-            try
-            {
+                int Srno, Description, Hsn, Gst, Pcs, SPrice, Rate, Per,Discount, Amount;
                 //Start Excel and get Application object.
                 oXL = new Excel.Application();
                 oXL.Visible = true;
@@ -72,46 +75,171 @@ namespace Invoice
                 oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
                 oSheet = (Excel._Worksheet)oWB.ActiveSheet;
 
-                //Add table headers going cell by cell.
-                oSheet.Cells[1, 2] = "First Name";
-                oSheet.Cells[1, 3] = "Last Name";
-                oSheet.Cells[1, 4] = "Full Name";
-                oSheet.Cells[1, 5] = "Salary";
-                oSheet.Cells[1, 6] = "Tax";
-
-                //Format A1:D1 as bold, vertical alignment = center.
-                oSheet.get_Range("A1", "D1").Font.Bold = true;
-                oSheet.get_Range("A1", "D1").VerticalAlignment =
+                #region "My Work"
+                oSheet.Cells[1, 10] = "TAX INVOICE";
+                oSheet.get_Range("A1", "J1").Font.Bold = true;
+                oSheet.get_Range("A1", "J1").Font.Size = 12;
+                oSheet.get_Range("A1", "J1").VerticalAlignment =
                 Excel.XlVAlign.xlVAlignCenter;
+                oSheet.get_Range("A1", "J1").HorizontalAlignment=
+                Excel.XlHAlign.xlHAlignCenter;
+                oSheet.get_Range("A1", "J1").Merge();
+                oSheet.Range["A1", "J1"].RowHeight = 24;
 
-                // Create an array to multiple values at once.
-                string[,] saNames = new string[5, 2];
+                //Seller Address
+                xRow = 2;
+                oSheet.Cells[xRow, 1] = "ANKIT STORE";
+                oSheet.get_Range("A2", "c2").Font.Bold = true;
+                oSheet.get_Range("A"+xRow.ToString(), "C"+xRow.ToString()).Merge();
 
-                saNames[0, 0] = "John";
-                saNames[0, 1] = "Smith";
-                saNames[1, 0] = "Tom";
-                saNames[1, 1] = "Brown";
-                saNames[2, 0] = "Sue";
-                saNames[2, 1] = "Thomas";
-                saNames[3, 0] = "Jane";
-                saNames[3, 1] = "Jones";
-                saNames[4, 0] = "Adam";
-                saNames[4, 1] = "Johnson";
+                //Invoice No
+                oSheet.Cells[xRow, 4] = "Invoice No: "+TxtInvoiceNo.Text;
+                oSheet.get_Range("D"+xRow.ToString(), "F"+(xRow+1).ToString()).Merge();
+                oSheet.get_Range("D2", "F3").HorizontalAlignment =
+                Excel.XlHAlign.xlHAlignCenter;
+                oSheet.get_Range("D2", "F3").VerticalAlignment =
+                Excel.XlVAlign.xlVAlignCenter;
+                //Date
+                DateTime dt = new DateTime();
+                dt = DatePicker.Value;
+                oSheet.Cells[xRow, 7] = "Dated " + dt.ToShortDateString(); 
+                oSheet.get_Range("G" + xRow.ToString(), "J" + (xRow+1).ToString()).Merge();
+                oSheet.get_Range("G2", "J3").HorizontalAlignment =
+               Excel.XlHAlign.xlHAlignCenter;
+                oSheet.get_Range("G2", "J3").VerticalAlignment =
+                Excel.XlVAlign.xlVAlignCenter;
+                xRow = 3;
+                oSheet.Cells[xRow, 1] = "71, Triveni Nagar 1st, Jaipur";
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
 
-                //Fill A2:B6 with an array of values (First and Last Names).
-                oSheet.get_Range("A2", "B6").Value2 = saNames;
+                xRow = 4;
+                oSheet.Cells[xRow, 1] = "Ph. - 8000262869";
+                oSheet.get_Range("A"+xRow.ToString(), "C"+xRow.ToString()).Merge();
 
-                //Fill C2:C6 with a relative formula (=A2 & " " & B2).
-                oRng = oSheet.get_Range("C2", "C6");
-                oRng.Formula = "=A2 & \" \" & B2";
+                xRow = 5;
+                oSheet.Cells[xRow, 1] = "GSTIN/UIN: 08HLVPD9817B1ZO";
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
 
-                //Fill D2:D6 with a formula(=RAND()*100000) and apply format.
-                oRng = oSheet.get_Range("D2", "D6");
-                oRng.Formula = "=RAND()*100000";
-                oRng.NumberFormat = "$0.00";
+                xRow = 6;
+                oSheet.Cells[xRow, 1] = "State Name: Rajasthan, Code : 08";
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
+
+                xRow = 7;
+                oSheet.Cells[xRow, 1] = "E - Mail: ankitramola101 @gmail.com";
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
+
+                //oSheet.get_Range("A2", "C7").Merge();
+                //lable
+                xRow = 8;
+                oSheet.Cells[xRow, 1] = "Bayer";
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
+
+                //Buyer Address
+                xRow = 9;
+                oSheet.Cells[xRow, 1] = TxtCompanyName.Text;
+                oSheet.get_Range("A9", "c9").Font.Bold = true;
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
+
+                xRow = 10;
+                oSheet.Cells[xRow, 1] = TxtAddress.Text;
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
+
+                xRow = 11;
+                oSheet.Cells[xRow, 1] = "E - Mail: "+ TxtEmail.Text ;
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
+
+                xRow = 12;
+                oSheet.Cells[xRow, 1] = "State : "+CmbState.Text.ToString()+", Code : "+TxtStateCode.Text;
+                oSheet.get_Range("A" + xRow.ToString(), "C" + xRow.ToString()).Merge();
+
+                xRow = 13;
+                //Grid Data
+                xRow += 1;
+
+                Srno = xCol;
+                oSheet.Cells[xRow, Srno] = "Sr No";
+                
+                xCol += 1;
+                Description = xCol;
+                oSheet.Cells[xRow, Description] = "Description of Goods";
+
+                xCol += 1;
+                Hsn = xCol;
+                oSheet.Cells[xRow, Hsn] = "HSN/SAC";
+
+                xCol += 1;
+                Gst= xCol;
+                oSheet.Cells[xRow, Gst] = "GST";
+
+                xCol += 1;
+                Pcs = xCol;
+                oSheet.Cells[xRow, Pcs] = "Pcs";
+
+                xCol += 1;
+                SPrice= xCol;
+                oSheet.Cells[xRow, Pcs] = "Sale Price";
+
+                xCol += 1;
+                Rate= xCol;
+                oSheet.Cells[xRow, Rate] = "Rate";
+
+                xCol += 1;
+                Per = xCol;
+                oSheet.Cells[xRow, Per] = "per";
+
+                xCol += 1;
+                Discount = xCol;
+                oSheet.Cells[xRow, Discount] = "Disc %";
+
+                xCol += 1;
+                Amount= xCol;
+                oSheet.Cells[xRow, Amount] = "Amount";
+
+                #endregion
+
+
+
+                ////Add table headers going cell by cell.
+                //oSheet.Cells[1, 2] = "First Name";
+                //oSheet.Cells[1, 3] = "Last Name";
+                //oSheet.Cells[1, 4] = "Full Name";
+                //oSheet.Cells[1, 5] = "Salary";
+                //oSheet.Cells[1, 6] = "Tax";
+
+                ////Format A1:D1 as bold, vertical alignment = center.
+                //oSheet.get_Range("A1", "D1").Font.Bold = true;
+                //oSheet.get_Range("A1", "D1").VerticalAlignment =
+                //Excel.XlVAlign.xlVAlignCenter;
+
+                //oSheet.get_Range("A1", "D1").Merge();
+                //// Create an array to multiple values at once.
+                //string[,] saNames = new string[5, 2];
+
+                //saNames[0, 0] = "John";
+                //saNames[0, 1] = "Smith";
+                //saNames[1, 0] = "Tom";
+                //saNames[1, 1] = "Brown";
+                //saNames[2, 0] = "Sue";
+                //saNames[2, 1] = "Thomas";
+                //saNames[3, 0] = "Jane";
+                //saNames[3, 1] = "Jones";
+                //saNames[4, 0] = "Adam";
+                //saNames[4, 1] = "Johnson";
+
+                ////Fill A2:B6 with an array of values (First and Last Names).
+                //oSheet.get_Range("A2", "B6").Value2 = saNames;
+
+                ////Fill C2:C6 with a relative formula (=A2 & " " & B2).
+                //oRng = oSheet.get_Range("C2", "C6");
+                //oRng.Formula = "=A2 & \" \" & B2";
+
+                ////Fill D2:D6 with a formula(=RAND()*100000) and apply format.
+                //oRng = oSheet.get_Range("D2", "D6");
+                //oRng.Formula = "=RAND()*100000";
+                //oRng.NumberFormat = "$0.00";
 
                 //AutoFit columns A:D.
-                oRng = oSheet.get_Range("A1", "D1");
+                oRng = oSheet.get_Range("A1", "A110");
                 oRng.EntireColumn.AutoFit();
 
 
